4ab23c342fa0e44bcd959dab54e177453077aeda
AkshayPatel09 <akshaydpatel01@gmail.com>
2022-06-23 12:32:18 +0530

    Complete navigation drawerlist and logout logic

--- app/src/main/java/com/example/final_main/HomeActivity.java
+++ app/src/main/java/com/example/final_main/HomeActivity.java
@@ -1,5 +1,7 @@
 package com.example.final_main;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -9,6 +11,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.final_main.ui.gallery.GalleryFragment;
+import com.example.final_main.ui.home.HomeFragment;
 import com.example.final_main.ui.slideshow.SlideshowFragment;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -79,6 +82,7 @@ public class HomeActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -91,6 +95,7 @@ public class HomeActivity extends AppCompatActivity {
                     transaction.replace(R.id.flNav, historyfragment);
                     transaction.addToBackStack(null);
                     transaction.commit();
+                    drawer.closeDrawers();
                 }
                 else if(item.getItemId() == R.id.nav_settings)
                 {
@@ -100,6 +105,7 @@ public class HomeActivity extends AppCompatActivity {
                     transaction.replace(R.id.flNav, galleryFragment);
                     transaction.addToBackStack(null);
                     transaction.commit();
+                    drawer.closeDrawers();
                 }
                 else if(item.getItemId() == R.id.nav_contactus)
                 {
@@ -109,6 +115,15 @@ public class HomeActivity extends AppCompatActivity {
                     transaction.replace(R.id.flNav, slideshowFragment);
                     transaction.addToBackStack(null);
                     transaction.commit();
+                    drawer.closeDrawers();
+
+                }
+                else if(item.getItemId()==R.id.nav_logOut){
+                    Toast.makeText(HomeActivity.this, "Logout!!", Toast.LENGTH_SHORT).show();
+                    logOut();
+                    drawer.closeDrawers();
+                    Intent intent = new Intent(HomeActivity.this,MainActivity.class);
+                    startActivity(intent);
 
                 }
                 return false;
@@ -129,5 +144,15 @@ public class HomeActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+    private void logOut(){
+        SharedPreferences sharedPreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(FIRSTNAME,"");
+        editor.putString(LASTNAME,"");
+        editor.putString(EMAIL,"");
+        editor.putString(PHONE,"");
+        editor.putString(ISLOGGEDIN,"false");
+        editor.commit();
     }
 }
